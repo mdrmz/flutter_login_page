e0fb9ba2cbedb258d5ee425a5c78164ba97c5e2c
mdrmz <drmzqalfa@gmail.com>
2022-11-12 03:26:38 +0300

    Update login_page.dart

--- start01/lib/login_page.dart
+++ start01/lib/login_page.dart
@@ -8,27 +8,72 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String UserName = "";
+  String Pass = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
+      backgroundColor: Color.fromARGB(255, 234, 234, 234),
       resizeToAvoidBottomInset: false,
       body: Form(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-            child: TextFormField(
+          child: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              autovalidateMode: AutovalidateMode
+                  .always, // kullancı adı girin diye uyarı veren yere bakıyor valdator içine gifer
               decoration: InputDecoration(
                 focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black)),
                 labelText: "Kullancı adı",
-                labelStyle: TextStyle(color: Colors.white),
+                labelStyle: TextStyle(color: Color.fromARGB(255, 6, 6, 6)),
               ),
+              validator: (value) {
+                // kulancı adı yazan yere girilen degeri tutuyor
+                if (value!.isEmpty) {
+                  return "Kullanıcı Adı Gir";
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (value) {
+                UserName = value!;
+              },
             ),
-          )
-        ],
+            SizedBox(height: 10.0),
+            TextFormField(
+              autovalidateMode: AutovalidateMode
+                  .always, // pass girin diye uyarı veren yere bakıyor valdator içine gifer
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black)),
+                labelText: "Şifre",
+                labelStyle: TextStyle(color: Color.fromARGB(255, 6, 6, 6)),
+              ),
+              validator: (value) {
+                //pass yazan yere girilen degeri tutuyor
+                if (value!.isEmpty) {
+                  return "şifre Gir";
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (value) {
+                Pass = value!;
+              },
+            ),
+            Row(
+              children: <Widget>[
+                MaterialButton(child: Text(" Üye Ol"), onPressed: () {}),
+                SizedBox(width: 200.0),
+                MaterialButton(child: Text("Şifremi Unuttum"), onPressed: () {})
+              ],
+            )
+          ],
+        ),
       )),
     );
   }
